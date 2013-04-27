cdaf314716b38d565bcfbb5e6f92deaf154e18ad
Jorjon <jorjon@jorjon.com>
2013-04-27 13:22:03 -0300

    Fixed compilation error

--- compile.bat
+++ compile.bat
@@ -1,2 +1,5 @@
+del bin\Darkjon.fdz
 copy /Y src\$(BaseDir)\Settings\Themes\Darkjon.fdi src\$(BaseDir)\Settings\Themes\CURRENT
-"c:\Program Files\7-Zip\7z" a -r -aoa -tzip bin\Darkjon.fdz $(BaseDir)
+xcopy  src\$(BaseDir) %TEMP%\$(BaseDir) /e /i /h
+"c:\Program Files\7-Zip\7z" a -r -tzip bin\Darkjon.fdz %TEMP%\$(BaseDir)
+rmdir /S /Q %TEMP%\$(BaseDir)
